5628b5f4cf8d1ffe6426596111113063b798c442
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-10-14 17:10:02 +0700

    add dashboard

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+const PageLogin = () => {
+
+
+  return (
+    <>
+      <div className="flex flex-rows w-screen">
+        <div className="lg:flex md:w-screen hidden">
+            Test
+        </div>
+        <div className="flex w-screen">
+            One
+        </div>
+        <div className="lg:flex md:w-screen hidden">
+            Two
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default PageLogin;
