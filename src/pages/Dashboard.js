f6768c1d007f9c9e07eeb9913408408cb4b8850b
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-10-17 19:37:32 +0700

    add rute bikun ui

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 
 import Maps from "../components/maps/Maps";
 
+import ruteBikun from "../assets/rute-bikun-only.png";
+
 const Dashboard = () => {
 
 
@@ -14,8 +16,9 @@ const Dashboard = () => {
                 <div className="flex lg:w-1/3 w-screen">
                     <Maps />
                 </div>
-                <div className="lg:flex lg:w-1/3 hidden">
-                    Two
+                <div className="lg:flex flex-col lg:w-1/3 hidden h-screen items-center justify-center">
+                    <p className="text-lg text-center">Rute Bikun UI</p>
+                    <img className="w-10/12" src={ruteBikun}></img>
                 </div>
 
             </div>
