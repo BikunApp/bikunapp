526910b0c865295dd7aed27fb3f452622fbb33ff
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-10-17 19:48:54 +0700

    change route call and maps sizing

---
+++ src/components/info/RouteInfo.js
@@ -0,0 +1,16 @@
+import ruteBikun from "../../assets/rute-bikun-only.png";
+
+const RouteInfo = () => {
+
+
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center">
+                <p className="text-lg">Rute Bikun UI</p>
+                <img className="w-10/12" src={ruteBikun}></img>
+            </div>
+        </>
+    );
+};
+
+export default RouteInfo;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,26 +1,23 @@
 import { useState, useEffect } from "react";
 
 import Maps from "../components/maps/Maps";
-
-import ruteBikun from "../assets/rute-bikun-only.png";
+import RouteInfo from "../components/info/RouteInfo";
 
 const Dashboard = () => {
 
 
     return (
         <>
-            <div className="flex flex-rows w-screen">
+            <div className="flex flex-rows h-screen w-screen">
                 <div className="lg:flex lg:w-1/3 hidden">
                     test
                 </div>
                 <div className="flex lg:w-1/3 w-screen">
                     <Maps />
                 </div>
-                <div className="lg:flex flex-col lg:w-1/3 hidden h-screen items-center justify-center">
-                    <p className="text-lg text-center">Rute Bikun UI</p>
-                    <img className="w-10/12" src={ruteBikun}></img>
+                <div className="lg:flex lg:w-1/3 h-screen w-screen hidden">
+                    <RouteInfo />
                 </div>
-
             </div>
         </>
     );
