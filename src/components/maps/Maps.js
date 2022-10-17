ee0668d930317291676d5bddcab28a81e4af05f5
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-10-17 18:24:09 +0700

    adjust menu size and add halte toggle

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -34,6 +34,7 @@ import halteMerah from '../../data/halteMerah.json';
 //Icons
 import RouteIcon from '@mui/icons-material/Route';
 import ForkLeftIcon from '@mui/icons-material/ForkLeft';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import PlaceIcon from '@mui/icons-material/Place';
 
 const Gmaps = () => {
@@ -42,7 +43,7 @@ const Gmaps = () => {
 
     const routeRef = useRef();
     const [route, setRoute] = useState(jalurMerah);
-    const [currentColor, setCurrentColor] = useState("merah");
+    const [halte, setHalte] = useState("merah");
 
     useEffect(() => {
         console.log(halteMerah[0]);
@@ -72,13 +73,20 @@ const Gmaps = () => {
         if (route == jalurMerah) {
 
             setRoute(jalurBiru);
-            setCurrentColor("biru");
+            if (halte != null) {
+
+                setHalte("biru");
+
+            }
 
         } else {
 
             setRoute(jalurMerah);
-            setCurrentColor("merah");
+            if (halte != null) {
 
+                setHalte("merah");
+
+            }
         }
     }
 
@@ -91,6 +99,26 @@ const Gmaps = () => {
         } else {
 
             setRoute(null);
+
+        }
+    }
+
+    var handleHalte = (e) => {
+
+        if (halte == null) {
+
+            if (route == jalurMerah) {
+
+                setHalte("merah");
+
+            } else {
+
+                setHalte("biru");
+
+            }
+        } else {
+
+            setHalte(null);
 
         }
     }
@@ -139,7 +167,7 @@ const Gmaps = () => {
                             leaveFrom="transform opacity-100 scale-100"
                             leaveTo="transform opacity-0 scale-95"
                         >
-                            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <Menu.Items className="absolute right-0 mt-2 w-28 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                 <div className="px-1 py-1">
                                     <Menu.Item>
                                         {({ active }) => (
@@ -149,7 +177,20 @@ const Gmaps = () => {
                                                 onClick={() => handleRoute()}
                                             >
                                                 <RouteIcon />
-                                                Toggle Rute
+                                                Rute
+                                            </button>
+                                        )}
+                                    </Menu.Item>
+
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <button
+                                                className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} 
+                                                group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                onClick={() => handleHalte()}
+                                            >
+                                                <DirectionsBusIcon />
+                                                Halte
                                             </button>
                                         )}
                                     </Menu.Item>
@@ -169,15 +210,27 @@ const Gmaps = () => {
 
                 <GeoJSON data={route} ref={routeRef} style={{ color: 'red' }} />
 
-                {currentColor == "merah" ?
+                {halte == "merah" ?
                     halteMerah.map(lokasi => (
+
                         <Marker icon={busStop} position={[lokasi.coordinate[1], lokasi.coordinate[0]]}>
                             <Popup>
                                 Halte <br></br>
                                 {lokasi.namaHalte}
                             </Popup>
                         </Marker>
-                    )) : null
+
+                    )) : halte == "biru" ?
+                        halteMerah.map(lokasi => (
+
+                            <Marker icon={busStop} position={[lokasi.coordinate[1], lokasi.coordinate[0]]}>
+                                <Popup>
+                                    Halte <br></br>
+                                    {lokasi.namaHalte}
+                                </Popup>
+                            </Marker>
+
+                        )) : null
                 }
 
 
