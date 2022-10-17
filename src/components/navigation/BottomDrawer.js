89b48c92b962b9fc67ac8fe26f59bfbb0cc2138a
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-10-17 20:16:30 +0700

    handle information route_

--- src/components/navigation/BottomDrawer.js
+++ src/components/navigation/BottomDrawer.js
@@ -5,11 +5,20 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
+import RouteInfo from "../info/RouteInfo";
+
 //Icons
 import DirectionsIcon from '@mui/icons-material/Directions';
 
 export default function SimpleBottomNavigation() {
     const [value, setValue] = React.useState(0);
+
+    var handleInformasiRute = (e) => {
+
+        
+
+
+    }
 
     return (
         <>
@@ -17,13 +26,9 @@ export default function SimpleBottomNavigation() {
                 <Box sx={{ width: '100vw' }}>
                     <BottomNavigation
                         showLabels
-                        value={value}
-                        onChange={(event, newValue) => {
-                            setValue(newValue);
-                        }}
                     >
                         <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-                        <BottomNavigationAction label="Informasi Rute" icon={<DirectionsIcon />} />
+                        <BottomNavigationAction label="Informasi Rute" onClick={() => handleInformasiRute()} icon={<DirectionsIcon />} />
                     </BottomNavigation>
                 </Box>
             </div>
