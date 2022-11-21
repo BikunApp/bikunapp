7c3c15a590db1e5d03d5071180ed7a602c6f148a
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-11-21 19:23:06 +0700

    remove unused code

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -1,9 +1,6 @@
-import { Fragment, useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
-import Backdrop from '@mui/material/Backdrop';
-
-import About from '../info/About';
 import './Maps.css';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,10 +13,6 @@ import {
     GeoJSON,
 } from 'react-leaflet'
 import L from 'leaflet';
-
-//headless ui
-import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 //MUI material
 import Button from '@mui/material/Button';
@@ -44,47 +37,8 @@ import blueBusIcon from '../../assets/icons/bus-icon-blue.png';
 import redStopIcon from '../../assets/icons/bus-stop-red.png';
 import blueStopIcon from '../../assets/icons/bus-stop-blue.png';
 
-
-import io from 'socket.io-client';
-
 import * as mqtt from 'react-paho-mqtt';
 import axios from 'axios';
-
-// //websocket connection
-// const socket = io('wss://bikunapp-backend.onrender.com', {
-//     withCredentials: true
-// });
-
-// //connection with server
-// socket.on('connect', () => {
-
-//     console.log('Connected to Server');
-//     clearTimeout(socket._connectTimer);
-
-// });
-
-// //when disconnected
-// socket.on('disconnect', function () {
-
-//     console.log('Disconnect from server')
-
-// });
-
-// socket._connectTimer = setTimeout(function () {
-
-//     socket.close();
-//     toast.error('Error connecting to server', {
-//         position: "top-right",
-//         autoClose: 8000,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//         progress: undefined,
-//         theme: "light",
-//     });
-
-// }, 10000);
 
 let firstTimeSub = 0;
 
@@ -175,8 +129,6 @@ const Maps = () => {
 
     }
 
-
-
     // called when subscribing topic(s)
     const _onSubscribe = () => {
         client.connect({
@@ -191,11 +143,6 @@ const Maps = () => {
 
         firstTimeSub++;
     }
-
-    //Websocket connection
-    // socket.on('broadcast', (message) => {
-    //     setTheSocketMessage(message.coordinate);
-    // });
 
     // Parse the incoming message from IoT
     var parseIncomingMessage = (message) => {
@@ -249,8 +196,6 @@ const Maps = () => {
 
         checkBusTimeout();
 
-        // setTheSocketMessage(JSON.parse(message.payloadString).coordinate);
-
     }
 
     // check if last time bus send data not more than 1 minute
@@ -301,6 +246,7 @@ const Maps = () => {
             }
 
             console.log("halte" + theHalte + " dalam waktu " + nearest + " detik");
+
         })
     }
 
@@ -365,20 +311,6 @@ const Maps = () => {
 
     }
 
-    const [ruteOpen, setRuteOpen] = useState(false);
-
-    const handleInformasiRuteClose = () => {
-
-        setRuteOpen(false);
-
-    };
-
-    const handleInformaiRute = () => {
-
-        setRuteOpen(!ruteOpen);
-
-    };
-
     //Leaflet Icons
     const redBus = L.icon({
         iconUrl: redBusIcon,
@@ -423,94 +355,13 @@ const Maps = () => {
     return (
         <>
             {console.log(currentBus)}
-            
-            {/* <div className="absolute z-[1001] pt-2 lg:right-1/3 right-0 pr-2">
-                <div className="flex space-x-2">
-                    <Button variant="contained" size="medium" style={{ backgroundColor: "#FFFFFF" }} onClick={() => handleChangeRoute()}> <ForkLeftIcon style={{ color: "#000000" }} /> </Button>
-                    <Menu as="div" className="relative inline-block text-left">
-                        <div>
-                            <Menu.Button className="inline-flex w-full justify-center rounded-sm bg-white bg-opacity-100 px-4 py-2 text-sm font-medium text-black hover:bg-slate-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-opacity-75 ring-2 ring-slate-300">
-                                Menu
-                                <ChevronDownIcon
-                                    className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
-                                    aria-hidden="true"
-                                />
-                            </Menu.Button>
-                        </div>
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                        >
-                            <Menu.Items className="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                <div className="px-1 py-1">
-                                    <Menu.Item>
-                                        {({ active }) => (
-                                            <button
-                                                className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} 
-                                                group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                                onClick={() => handleRoute()}
-                                            >
-                                                <RouteIcon />
-                                                Rute
-                                            </button>
-                                        )}
-                                    </Menu.Item>
-
-                                    <Menu.Item>
-                                        {({ active }) => (
-                                            <button
-                                                className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} 
-                                                group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                                onClick={() => handleHalte()}
-                                            >
-                                                <DirectionsBusIcon />
-                                                Halte
-                                            </button>
-                                        )}
-                                    </Menu.Item>
-
-                                    <Menu.Item>
-                                        {({ active }) => (
-                                            <button
-                                                className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} 
-                                                group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                                onClick={() => handleResetView()}
-                                            >
-                                                <RestartAltIcon />
-                                                Reset Tampilan
-                                            </button>
-                                        )}
-                                    </Menu.Item>
-
-                                    <Menu.Item>
-                                        {({ active }) => (
-                                            <button
-                                                className={`${active ? 'bg-violet-500 text-white' : 'text-gray-900'} 
-                                                group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                                onClick={handleInformaiRute}
-                                            >
-                                                <InfoOutlinedIcon />
-                                                Tentang
-                                            </button>
-                                        )}
-                                    </Menu.Item>
-                                </div>
-                            </Menu.Items>
-                        </Transition>
-                    </Menu>
-                </div>
-            </div> */}
 
             <MapContainer center={mapCenter} zoom={mapZoom} scrollWheelZoom={true} zoomControl={false} ref={mainRef}>
 
                 <TileLayer
                     attribution='&copy; <a href="https://www.google.com/help/legalnotices_maps/">Google</a> Maps'
-                    url="http://mt0.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+                    url="https://mt0.google.com/vt?lyrs=m&x={x}&y={y}&z={z}"
+                // className='map-tiles'
                 />
 
                 {route === jalurMerah ?
@@ -552,23 +403,6 @@ const Maps = () => {
                             </Popup>
                         </Marker>))}
             </MapContainer>
-
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={ruteOpen}
-                onClick={handleInformasiRuteClose}
-            >
-
-                <div className="flex h-2/3 items-center justify-center">
-                    <div className="bg-white py-5 w-11/12 sm:w-3/5 md:w-3/6 lg:w-3/5 rounded-xl shadow-xl drop-shadow-sm">
-
-                        <About />
-                    </div>
-
-                </div>
-
-
-            </Backdrop>
 
             <ToastContainer
                 position="top-right"
