90cc72634f2fbc9ced8d24365a46c5b296d3e52f
zuhalal <zuhalalimulhadi19@gmail.com>
2022-11-27 11:06:47 +0700

    feat: about us card component

---
+++ src/components/elements/card/TeamInfoCard.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export const TeamInfoCard = ({
+  photo,
+  name,
+  role,
+  githubLink,
+  websiteLink,
+  linkedinLink,
+  faculty,
+  year,
+  team,
+}) => {
+  return (
+    <section className="shadow-cardTeam flex justify-between gap-6 rounded-lg p-4 overflow-auto">
+      <section className="flex gap-3">
+        <div className="flex items-center justify-center h-full">
+          <div class="relative md:w-24 md:h-24 w-12 h-12">
+            <img
+              class="rounded-full border border-gray-100 shadow-sm"
+              src={
+                photo
+                  ? process.env.PUBLIC_URL + photo
+                  : "https://randomuser.me/api/portraits/women/81.jpg"
+              }
+              alt="team profile"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col justify-between">
+          <section>
+            <p className="text-poppins-p font-bold">{name ?? ""}</p>
+            <p className="text-poppins-p-small">{role ?? ""}</p>
+          </section>
+          <section className="flex gap-4 items-center">
+            {githubLink ? (
+              <a href={githubLink}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/icons/github.svg`}
+                  alt="github"
+                />
+              </a>
+            ) : null}
+
+            {linkedinLink ? (
+              <a href={linkedinLink}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/icons/linkedin.svg`}
+                  alt="linkedin"
+                />
+              </a>
+            ) : null}
+
+            {websiteLink ? (
+              <a href={websiteLink}>
+                <img
+                  src={`${process.env.PUBLIC_URL}/icons/globe.svg`}
+                  alt="globe"
+                />
+              </a>
+            ) : null}
+          </section>
+        </div>
+      </section>
+      <section className="flex flex-col items-end justify-between gap-4">
+        <img
+          src={`${process.env.PUBLIC_URL}/icons/${
+            team === "ristek" ? "ristek.svg" : "hive.svg"
+          } `}
+          alt="team"
+          width={48}
+        />
+        <p className="text-poppins-p-small text-[#9B9B9B]">
+          {faculty} {year}
+        </p>
+      </section>
+    </section>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      boxShadow: {
+        cardTeam: "0px 2px 8px rgba(0, 0, 0, 0.25)",
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
